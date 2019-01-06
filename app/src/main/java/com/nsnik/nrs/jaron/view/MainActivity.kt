1715592a490fb25d3005363725bf8204689960c8
nsnikhil <n.nikhil.ns65@gmail.com>
2019-01-06 12:39:32 +0530

    Add toolbar in main activity layout and initialize it in main activity

--- app/src/main/java/com/nsnik/nrs/jaron/view/MainActivity.kt
+++ app/src/main/java/com/nsnik/nrs/jaron/view/MainActivity.kt
@@ -2,12 +2,26 @@ package com.nsnik.nrs.jaron.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nsnik.nrs.jaron.BuildConfig
+import com.nsnik.nrs.jaron.MyApplication
 import com.nsnik.nrs.jaron.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initialize()
     }
+
+    private fun initialize() {
+        setSupportActionBar(mainToolbar)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (BuildConfig.DEBUG) MyApplication.getRefWatcher(this)?.watch(this)
+    }
+
 }
