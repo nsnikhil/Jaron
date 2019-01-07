c88e11d940459a58692525f4f00ef3b96de1d31b
nsnikhil <n.nikhil.ns65@gmail.com>
2019-01-07 11:00:17 +0530

    add temporary about menu in main activity

--- app/src/main/java/com/nsnik/nrs/jaron/view/MainActivity.kt
+++ app/src/main/java/com/nsnik/nrs/jaron/view/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.nsnik.nrs.jaron.view
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.nsnik.nrs.jaron.BuildConfig
 import com.nsnik.nrs.jaron.MyApplication
@@ -15,6 +17,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initialize()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.menuItemAbout -> {
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun initialize() {
