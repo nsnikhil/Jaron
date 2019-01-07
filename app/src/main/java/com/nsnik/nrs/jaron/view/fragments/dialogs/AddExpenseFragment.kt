afbd77864fdfa35689f96508f7331b89f1fa7c69
nsnikhil <n.nikhil.ns65@gmail.com>
2019-01-07 12:24:11 +0530

    add code in dialog code for creating new expense

--- app/src/main/java/com/nsnik/nrs/jaron/view/fragments/dialogs/AddExpenseFragment.kt
+++ app/src/main/java/com/nsnik/nrs/jaron/view/fragments/dialogs/AddExpenseFragment.kt
@@ -6,14 +6,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.ViewModelProviders
 import com.jakewharton.rxbinding2.view.RxView
 import com.nsnik.nrs.jaron.R
 import com.nsnik.nrs.jaron.util.ApplicationUtility.Companion.getFormattedText
+import com.nsnik.nrs.jaron.util.FieldValidator.Companion.validateFrom
+import com.nsnik.nrs.jaron.util.factory.ExpenseEntityFactory
 import com.nsnik.nrs.jaron.viewModel.ExpenseListViewModel
 import io.reactivex.disposables.CompositeDisposable
 import kotlinx.android.synthetic.main.fragment_add_expense.*
+import java.util.*
 
 
 class AddExpenseFragment : DialogFragment() {
@@ -53,17 +57,38 @@ class AddExpenseFragment : DialogFragment() {
                 dismiss()
             },
             RxView.clicks(newExpenseCreate).subscribe {
-
+                addExpense()
+                dismiss()
             }
         )
     }
 
     private fun addExpense() {
-
+        if (validateEntries()) expenseListViewModel.insertExpenses(
+            listOf(
+                ExpenseEntityFactory.createExpenseEntity(
+                    textViewToString(newExpenseValue).toDouble(),
+                    textViewToString(newExpenseTitle),
+                    textViewToString(newExpenseDescription),
+                    Calendar.getInstance().time,
+                    Collections.emptyList()
+                )
+            )
+        )
     }
 
     private fun validateEntries(): Boolean {
-        return false
+        return validateFrom(
+            textViewToString(newExpenseValue).toDouble(),
+            textViewToString(newExpenseTitle),
+            textViewToString(newExpenseDescription),
+            Calendar.getInstance().time,
+            Collections.emptyList()
+        )
+    }
+
+    private fun textViewToString(textView: TextView): String {
+        return textView.text.toString()
     }
 
     override fun onResume() {
