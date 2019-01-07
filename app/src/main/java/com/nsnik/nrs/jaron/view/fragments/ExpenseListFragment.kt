a40bae82dc890a5bbc7237a9ce9e1959c79e9bbe
nsnikhil <n.nikhil.ns65@gmail.com>
2019-01-07 12:40:54 +0530

    attach adapter and view model in expense list fragment

--- app/src/main/java/com/nsnik/nrs/jaron/view/fragments/ExpenseListFragment.kt
+++ app/src/main/java/com/nsnik/nrs/jaron/view/fragments/ExpenseListFragment.kt
@@ -5,9 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.jakewharton.rxbinding2.view.RxView
 import com.nsnik.nrs.jaron.R
+import com.nsnik.nrs.jaron.view.fragments.adapters.ExpenseListAdapter
 import com.nsnik.nrs.jaron.view.fragments.dialogs.AddExpenseFragment
+import com.nsnik.nrs.jaron.viewModel.ExpenseListViewModel
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.functions.Consumer
 import kotlinx.android.synthetic.main.fragment_expense_list.*
@@ -16,6 +22,8 @@ import kotlinx.android.synthetic.main.fragment_expense_list.*
 class ExpenseListFragment : Fragment() {
 
     private val compositeDisposable: CompositeDisposable = CompositeDisposable()
+    private lateinit var expenseListViewModel: ExpenseListViewModel
+    private lateinit var expenseListAdapter: ExpenseListAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_expense_list, container, false)
@@ -28,6 +36,17 @@ class ExpenseListFragment : Fragment() {
     }
 
     private fun initialize() {
+        expenseListViewModel = ViewModelProviders.of(this).get(ExpenseListViewModel::class.java)
+        expenseListAdapter = ExpenseListAdapter()
+
+        expenseFragmentExpenseList.apply {
+            layoutManager = LinearLayoutManager(activity, RecyclerView.VERTICAL, false)
+            adapter = expenseListAdapter
+        }
+
+        expenseListViewModel.getAllExpenses().observe(this, Observer {
+            expenseListAdapter.submitList(it)
+        })
     }
 
     private fun listeners() {
